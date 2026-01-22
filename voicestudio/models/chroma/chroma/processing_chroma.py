a256b8e48b633446abaabb55d3c71e685ed72bc8
chentanyu <cty13564970268@163.com>
2026-01-22 14:33:50 +0800

    feat: compact latest transformers

--- voicestudio/models/chroma/chroma/processing_chroma.py
+++ voicestudio/models/chroma/chroma/processing_chroma.py
@@ -1,21 +1,92 @@
-import numpy as np
+# coding=utf-8
+# Copyright 2025 The FlashLabs team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import base64
+import logging
+from io import BytesIO
+
+import av
 import torch
+import librosa
+import audioread
 import torchaudio
-from transformers import AutoProcessor
-from transformers.models.qwen2_5_omni import Qwen2_5OmniProcessor
+import numpy as np
+from typing import Union, Optional, Tuple, Unpack, List
+from transformers.models.qwen2_5_omni.processing_qwen2_5_omni import Qwen2_5OmniProcessor
 from transformers.processing_utils import AudioKwargs, ProcessingKwargs
-from qwen_omni_utils import process_audio_info
-from typing import Union, Optional, Tuple
 from transformers.feature_extraction_utils import BatchFeature
 
-try:
-    from typing import Unpack
-except ImportError:
-    from typing_extensions import Unpack
+
+def _check_if_video_has_audio(video_path):
+    container = av.open(video_path)
+    audio_streams = [stream for stream in container.streams if stream.type == "audio"]
+    if not audio_streams:
+        return False
+    return True
+
+
+def process_audio_info(conversations: list[dict] | list[list[dict]], use_audio_in_video: bool):
+    audios = []
+    if isinstance(conversations[0], dict):
+        conversations = [conversations]
+    for conversation in conversations:
+        for message in conversation:
+            if not isinstance(message["content"], list):
+                continue
+            for ele in message["content"]:
+                if ele["type"] == "audio":
+                    if "audio" in ele:
+                        path = ele["audio"]
+                        if isinstance(path, np.ndarray):
+                            if path.ndim > 1:
+                                raise ValueError("Support only mono audio")
+                            audios.append(path)
+                        elif path.startswith("data:audio"):
+                            _, base64_data = path.split("base64,", 1)
+                            data = base64.b64decode(base64_data)
+                            audios.append(librosa.load(BytesIO(data), sr=16000)[0])
+                        elif path.startswith("http://") or path.startswith("https://"):
+                            audios.append(librosa.load(audioread.ffdec.FFmpegAudioFile(path), sr=16000)[0])
+                        elif path.startswith("file://"):
+                            audios.append(librosa.load(path[len("file://"):], sr=16000)[0])
+                        else:
+                            audios.append(librosa.load(path, sr=16000)[0])
+                    else:
+                        raise ValueError("Unknown audio {}".format(ele))
+                if use_audio_in_video and ele["type"] == "video":
+                    if "video" in ele:
+                        path = ele["video"]
+                        assert _check_if_video_has_audio(
+                            path
+                        ), "Video must has audio track when use_audio_in_video=True"
+                        if path.startswith("http://") or path.startswith("https://"):
+                            audios.append(librosa.load(audioread.ffdec.FFmpegAudioFile(path), sr=16000)[0])
+                        elif path.startswith("file://"):
+                            audios.append(librosa.load(path[len("file://"):], sr=16000)[0])
+                        else:
+                            audios.append(librosa.load(path, sr=16000)[0])
+                    else:
+                        raise ValueError("Unknown video {}".format(ele))
+    if len(audios) == 0:
+        audios = None
+    return audios
 
 
 class ChromaAudioKwargs(AudioKwargs, total=False):
-    target_sample_rate: Optional[int]  # 目标采样率
+    target_sample_rate: Optional[int]
 
 
 class ChromaProcessorKwargs(ProcessingKwargs, total=False):
@@ -38,51 +109,80 @@ class ChromaProcessorKwargs(ProcessingKwargs, total=False):
 
 
 class ChromaProcessor(Qwen2_5OmniProcessor):
+    r"""
+        Constructs a Chroma processor inherit from Qwen2.5Omni processor.
+        [`ChromaProcessor`] offers all the functionalities of [`Qwen2VLImageProcessor`], [`WhisperFeatureExtractor`], and [`Qwen2TokenizerFast`]. See the
+        [`~ChromaProcessor.__call__`] and [`~ChromaProcessor.decode`] for more information.
+        Args:
+            image_processor ([`Qwen2VLImageProcessor`], *optional*):
+                The image processor.
+            video_processor ([`Qwen2VLVideoProcessor`], *optional*):
+                The video processor.
+            feature_extractor ([`WhisperFeatureExtractor`], *optional*):
+                The audio feature extractor.
+            tokenizer ([`Qwen2TokenizerFast`], *optional*):
+                The text tokenizer.
+            chat_template (`Optional[str]`, *optional*):
+                The Jinja template to use for formatting the conversation. If not provided, the default chat template is used.
+        """
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, image_processor=None, video_processor=None, feature_extractor=None, tokenizer=None,
+                 chat_template=None):
+        super().__init__(image_processor, video_processor, feature_extractor, tokenizer, chat_template)
 
-    def __call__(self, *args, **kwargs: Unpack[ChromaProcessorKwargs]) -> BatchFeature:
-        # thinker processor
-        text, audios = self.apply_chat_template(*args, **kwargs)
-        thinker_inputs = super().__call__(text=text, audio=audios, return_tensors="pt", padding=True,
-                                          use_audio_in_video=False)
+    def __call__(
+        self,
+        conversations: List[List[dict]],
+        prompt_audio: List[str],
+        prompt_text: List[str],
+        **kwargs: Unpack[ChromaProcessorKwargs]
+    ) -> BatchFeature:
 
-        inputs = {f"thinker_{k}": v for k, v in thinker_inputs.items()}
-
-        prompt_audio = kwargs.get("prompt_audio")
-        prompt_text = kwargs.get("prompt_text")
         assert prompt_audio is not None, "prompt_audio can not be empty"
         assert prompt_text is not None, "prompt_text can not be empty"
 
-        prompt_ids = super().__call__(text=prompt_text, return_tensors="pt")
+        N = len(conversations)
+        assert len(prompt_audio) == N, f"prompt_audio length {len(prompt_audio)} != conversations length {N}"
+        assert len(prompt_text) == N, f"prompt_text length {len(prompt_text)} != conversations length {N}"
 
-        if isinstance(prompt_audio, str):
-            prompt_audio_tensor = self.load_audio(prompt_audio, kwargs.get("target_sample_rate", 24000))
-        elif isinstance(prompt_audio, torch.Tensor):
-            prompt_audio_tensor = prompt_audio
-        elif isinstance(prompt_audio, np.ndarray):
-            prompt_audio_tensor = torch.from_numpy(prompt_audio)
-            if prompt_audio_tensor.dim() > 1:
-                prompt_audio_tensor = prompt_audio_tensor.squeeze()
-        else:
-            raise ValueError(f"prompt audio must be str, tensor or numpy, but got  {type(prompt_audio)}")
+        # thinker processor
+        text, audios = self.apply_chat_template(conversations, **kwargs)
+        thinker_inputs = super().__call__(
+            text=text,
+            audio=audios,
+            return_tensors="pt",
+            padding=True,
+            use_audio_in_video=False
+        )
+        thinker_inputs = {f"thinker_{k}": v for k, v in thinker_inputs.items()}
+
+        inputs = super().__call__(text=prompt_text, return_tensors="pt", padding=True)
+        prompt_audio_wavs = [self.load_audio(audio, kwargs.get("target_sample_rate", 24000)) for audio in prompt_audio]
+        prompt_audio_cutoffs = torch.tensor([len(audio) for audio in prompt_audio_wavs], dtype=torch.long)
+        prompt_audio_tensor = torch.nn.utils.rnn.pad_sequence(
+            prompt_audio_wavs, batch_first=True
+        ).unsqueeze(1)  # add channel dimension
 
         return BatchFeature(
             data={
+                **thinker_inputs,
                 **inputs,
-                **prompt_ids,
-                'input_values': prompt_audio_tensor
+                'input_values': prompt_audio_tensor,
+                'input_values_cutoffs': prompt_audio_cutoffs
             },
             tensor_type=kwargs.get("return_tensors"),
         )
 
-    def load_audio(self, audio_path: str, target_sample_rate: int = 24000) -> torch.Tensor:
-        """加载音频文件并重采样，支持本地路径和 HTTP/HTTPS URL"""
+    def load_audio(self, audio_path: str | None, target_sample_rate: int = 24000) -> torch.Tensor:
+        """
+        load audio wav and resample it to target sample rate
+        Args:
+            audio_path:
+            target_sample_rate:
+        Returns:
+        """
         try:
-            # 本地文件路径
             audio_tensor, sample_rate = torchaudio.load(audio_path)
-
             if audio_tensor.shape[0] > 1:
                 audio_tensor = torch.mean(audio_tensor, dim=0, keepdim=True)
 
@@ -94,7 +194,7 @@ class ChromaProcessor(Qwen2_5OmniProcessor):
 
             return audio_tensor
         except Exception as e:
-            print(f"加载音频文件失败: {audio_path}, 错误: {e}")
+            logging.error(f"load audio file erro: {e}")
             raise
 
     def apply_chat_template(
@@ -103,11 +203,18 @@ class ChromaProcessor(Qwen2_5OmniProcessor):
         chat_template=None,
         **kwargs
     ) -> Tuple[str, list]:
-        """应用聊天模板"""
+        """
+        apply chat_template.jinja template to format conversations
+        Args:
+            conversations:
+            chat_template:
+            **kwargs:
+        Returns:
+        """
         if isinstance(conversations[0], dict):
             conversations = [conversations]
         audios = process_audio_info(conversations, use_audio_in_video=False)
-        return super().apply_chat_template(conversations, chat_template, **kwargs), audios
+        return self.tokenizer.apply_chat_template(conversations, chat_template, **kwargs), audios
 
 
-AutoProcessor.register("ChromaProcessor", ChromaProcessor)
+__all__ = ["ChromaProcessor"]
