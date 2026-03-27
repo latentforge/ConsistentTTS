53db6a8625dd81bf2d9ffd6f70370d50691c7826
BREW <81044362+b-re-w@users.noreply.github.com>
2026-03-27 23:17:40 +0900

    Feat: Add Higgs Tokenizer

--- voicestudio/models/higgs_audio/__init__.py
+++ voicestudio/models/higgs_audio/__init__.py
@@ -1,5 +1,9 @@
 from .configuration_higgs_audio import HiggsAudioV2Config as HiggsAudioConfig
 from .processing_higgs_audio import HiggsAudioV2Processor as HiggsAudioProcessor
+from .tokenization_higgs_audio import (
+    HiggsAudioV2TokenizerModel as HiggsAudioTokenizerModel,
+    HiggsAudioV2TokenizerConfig as HiggsAudioTokenizerConfig
+)
 from .modeling_higgs_audio import (
     HiggsAudioV2ForConditionalGeneration as HiggsAudioForConditionalGeneration,
     HiggsAudioV2PreTrainedModel as HiggsAudioPreTrainedModel,
