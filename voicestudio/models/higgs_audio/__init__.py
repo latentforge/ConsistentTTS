30281965720c572dc9373446eba451fbfce18f3b
BREW <81044362+b-re-w@users.noreply.github.com>
2026-03-27 23:03:03 +0900

    Feat: Add Official Higgs Audio

--- voicestudio/models/higgs_audio/__init__.py
+++ voicestudio/models/higgs_audio/__init__.py
@@ -1,13 +1,7 @@
-from transformers import AutoConfig, AutoModel, AutoProcessor
-
-from .configuration_higgs_audio import HiggsAudioConfig, HiggsAudioEncoderConfig, HiggsAudioGenerationConfig
-from .modeling_higgs_audio import HiggsAudioModel, HiggsAudioForConditionalGeneration
-from .processing_higgs_audio import HiggsAudioProcessor
-
-
-AutoConfig.register("higgs_audio_encoder", HiggsAudioEncoderConfig)
-AutoConfig.register("higgs_audio", HiggsAudioConfig)
-#AutoConfig.register("higgs_audio_generation", HiggsAudioGenerationConfig)
-AutoModel.register(HiggsAudioConfig, HiggsAudioModel)
-AutoModel.register(HiggsAudioConfig, HiggsAudioForConditionalGeneration)
-AutoProcessor.register(HiggsAudioConfig, HiggsAudioProcessor)
+from .configuration_higgs_audio import HiggsAudioV2Config as HiggsAudioConfig
+from .processing_higgs_audio import HiggsAudioV2Processor as HiggsAudioProcessor
+from .modeling_higgs_audio import (
+    HiggsAudioV2ForConditionalGeneration as HiggsAudioForConditionalGeneration,
+    HiggsAudioV2PreTrainedModel as HiggsAudioPreTrainedModel,
+    HiggsAudioV2Model as HiggsAudioModel
+)
